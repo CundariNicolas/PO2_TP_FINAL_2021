737edfd2328aececc7a60ea519bda10a76fa7a2e
Nicolás Cundari <nicolasmcundari@gmail.com>
2021-06-03 15:47:41 -0300

    Cambio privacidad variables

--- src/publicacion/Foto.java
+++ src/publicacion/Foto.java
@@ -4,11 +4,11 @@ import java.util.Calendar;
 
 public class Foto {
 	
-	String path;
-	boolean marca360;
-	Calendar fechaCaptura;
-	Integer ancho;
-	Integer alto;
+	private String path;
+	private boolean marca360;
+	private Calendar fechaCaptura;
+	private Integer ancho;
+	private Integer alto;
 	
 	
 	public Foto(String path, boolean marca360, Calendar fecha, Integer ancho, Integer alto) {
