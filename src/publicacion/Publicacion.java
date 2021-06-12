7032faf4a1028051c503735ac33fb9c7d3268bfb
Nicolás Cundari <nicolasmcundari@gmail.com>
2021-06-11 21:16:04 -0300

    Publicacion: está hecho todo con sus tests. Falta terminar los objetos que se vinculan, como PrecioPeriodo para ver como implementar el informe de baja de precio

--- src/publicacion/Publicacion.java
+++ src/publicacion/Publicacion.java
@@ -3,6 +3,7 @@ package publicacion;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import inmueble.FormaDePago;
 import inmueble.Inmueble;
 
 public class Publicacion {
@@ -22,23 +23,157 @@ public class Publicacion {
 		this.fechaFin = fin;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		this.fotos = fotos;
-		this.precio = precio;
+		this.setFotos(fotos);
+		this.setPrecio(precio);
 		this.periodoOcupado = new ArrayList<Periodo>();
 		this.observadorBajaPrecio = new ArrayList<Observer>();
 		
 	}
 	
-	public void registrarOcupacion(Calendar inicio, Calendar fin) {
-		periodoOcupado.add(new Periodo(inicio, fin));
+	public void registrarOcupacion(Periodo periodoOcupacion) {
+		/**
+		 * Registra una ocupación en un periodo dado
+		 * El periodo dado no debería estar ocupado para poder llevarse a cabo
+		 */
+		if(this.estaDisponibleEntre(periodoOcupacion)) {
+			periodoOcupado.add(periodoOcupacion);
+		}
+		
 	}
-	public boolean estaDisponibleEntre(Calendar fechaDesde, Calendar fechaHasta) {
-		return true;
+	private boolean estaDisponibleEntre(Periodo periodo) {
+		/** Indica si está disponible en el periodo dado
+		 * Verdadero si el periodo a ocupar esta disponible, no debe coincidir con ningun periodo ocupado ya reservado
+		 * 
+		 */
+		return this.getPeriodosOcupados().stream().allMatch(ocupado -> this.estaLibreAntes(ocupado, periodo) || this.estaLibreDespues(ocupado, periodo));
 	}
+	
+	
+	/* estaLibreAntes */
+	boolean estaLibreAntes(Periodo ocupado, Periodo periodo) {
+		/**Indica si el periodo a ocupar esta libre antes del periodo ocupado, que no exista una interseccion entre los dias de los periodos
+		 * 
+		 */
+		return  this.elInicioAOcuparEstaAntesOElMismoDia(ocupado, periodo) && this.elFinEstaAntesDelInicioOcupado(ocupado, periodo) ;
+	}
+	
+	
+	
+	boolean elFinEstaAntesDelInicioOcupado(Periodo ocupado, Periodo periodo) {
+		return ocupado.getFechaInicio().after(periodo.getFechaFin());
+	}
+	boolean elInicioAOcuparEstaAntesOElMismoDia(Periodo ocupado, Periodo periodo) {
+		return ocupado.getFechaInicio().after(periodo.getFechaInicio());
+	}
+	
+	
+	
+	
+	
+	public ArrayList<FormaDePago> medioDePagoHabilitado(){
+		return inmueble.getFormaDePago();
+	}
+	
+	
+	
+	
+	
+	/* estaLibreDespues */
+	
+	boolean estaLibreDespues(Periodo ocupado, Periodo periodo) {
+		/**
+		 * indica si el periodo a ocupare sta libre despues del periodo ocupado, si es el mismo dia
+		 * se asume como desocupado, que no exista interseccion entre los dias de ambos periodos
+		 */
+		return this.elInicioAOcuparEstaDespuesOElMismoDia(ocupado, periodo) && this.elFinAOcuparEstaDespuesOElMismoDia(ocupado, periodo);
+	}
+	
+	/*  asd asd aasdads */
+	/** asd*/
+	
+	private boolean elFinAOcuparEstaDespuesOElMismoDia(Periodo ocupado, Periodo periodo) {
+		
+		return ocupado.getFechaFin().before(periodo.getFechaFin()) ;
+	}
+	
+	
+	
+	
+	
 
+	private boolean elInicioAOcuparEstaDespuesOElMismoDia(Periodo ocupado, Periodo periodo) {
+		/**
+		 * Indica si la fecha del fin del periodo ocupado esta antes del inicio del periodo o es el mismo dia
+		 */
+		return (ocupado.getFechaFin().before(periodo.getFechaInicio()) );
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public void notificarBajaDePrecio() {
+		this.observadorBajaPrecio.stream().forEach(obs -> obs.publish("Bajo el precio"));
+		
+	}
+	
+	public void suscribirBajaDePrecio(Observer observador1) {
+		this.observadorBajaPrecio.add(observador1);
+	}
+	
+	
+
+	
+	
+	
+	
+	
+	// GETTERS AND SETTERS
 	public ArrayList<Periodo> getPeriodosOcupados() {
 		
 		return periodoOcupado;
+	}
+
+	public Inmueble getInmueble() {
+		return inmueble;
+	}
+
+	public Calendar getFechaInicioPublicacion() {
+		return fechaInicio;
+	}
+
+	public Calendar getFechaFin() {
+		return fechaFin;
+	}
+
+	public Calendar getCheckIn() {
+		return checkIn;
+	}
+
+	public Calendar getCheckOut() {
+		return checkOut;
+	}
+
+	public ArrayList<Foto> getFotos() {
+		return fotos;
+	}
+
+	public void setFotos(ArrayList<Foto> fotos) {
+		this.fotos = fotos;
+	}
+
+	public ArrayList<PrecioPeriodo> getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(ArrayList<PrecioPeriodo> precio) {
+		this.precio = precio;
 	}
 	
 
