4ac5e11258277fa0df656a097ba8a5e222fbe939
Damian Penalver <jdpenalver@hotmail.com>
2021-06-24 15:51:25 -0300

    Tipo inmueble comentarios

--- src/tipoInmueble/TipoDeInmueble.java
+++ src/tipoInmueble/TipoDeInmueble.java
@@ -4,10 +4,17 @@ public class TipoDeInmueble {
 
 	String descripcion;
 	
+	/**
+	 *  Contructor de la clase TipoDeInmueble 
+	 * @param descripcion String que represetna el tipo de inmueble por ejemplo "PH" o "Departamento"
+	 */
 	public TipoDeInmueble(String descripcion) {
 		this.setDescripcion(descripcion);
 	}
-
+	/**
+	 * 
+	 * @return String que represetna el tipo de inmueble por ejemplo "PH" o "Departamento" 
+	 */
 	public String codigoDescripcion() {
 		return this.descripcion;
 	}
