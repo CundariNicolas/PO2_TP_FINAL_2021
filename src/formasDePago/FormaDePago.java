7e77dcbdea7749b470a8a6351dde00e64ee2aecb
Damian Penalver <jdpenalver@hotmail.com>
2021-06-24 15:02:24 -0300

    forma de pago comentarios

--- src/formasDePago/FormaDePago.java
+++ src/formasDePago/FormaDePago.java
@@ -5,22 +5,47 @@ public class FormaDePago {
 	private String codigo;
 	private String descripcion;
 	
+	/**
+	 * Constructor de una Forma de Pago 
+	 * Representa a las fomar de pago como Tarjeta de Debito , MercadoPago, etc 
+	 * @param codigo String
+	 * @param descripcion String 
+	 */
 	public FormaDePago (String codigo, String descripcion) {
 		this.setDescripcion(descripcion);
 		this.setSetCodigo(codigo);
 	}
 
+	/**
+	 * Codigo interno de referencia sobre la forma de pago
+	 * 
+	 * @param codigo String
+	 */
 	public void setSetCodigo(String codigo) {
 		this.codigo = codigo;
 	}
 	
+	/**
+	 *  configura del metodo de pago representado por un String 
+	 * @param descripcion
+	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
 
+	
+	/**
+	 * 
+	 * @return String que representa el cofigo de la forma de pago
+	 */
 	public String getCodigo() {
 		return this.codigo;
 	}
+	
+	/**
+	 *  
+	 * @return String representa la descripcion de la forma de pago Ejem "Mercado Libre", "Tarjeta de Debito", etc..
+	 */
 
 	public String getDescripcion() {
 		return this.descripcion;
