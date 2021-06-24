87d2cec1e635e5cda6ff94cb375bffb32805074a
Nicolás Cundari <nicolasmcundari@gmail.com>
2021-06-24 01:46:33 -0300

    Elimine algo innecesario de calificable

--- src/calificacion/Calificable.java
+++ src/calificacion/Calificable.java
@@ -2,13 +2,11 @@ package calificacion;
 
 import java.util.List;
 
-import usuario.Usuario;
-
 
 public interface Calificable {
 	
 	public void setCalificacion(Calificacion unaCalificaion);
 	public List <Calificacion> getCalificaciones();
-	int compareTo(Usuario o);
+	
 
 }
