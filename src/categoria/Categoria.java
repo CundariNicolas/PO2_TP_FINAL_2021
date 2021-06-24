5c9c49002b59d05903b771479af7835f0002760e
Damian Penalver <jdpenalver@hotmail.com>
2021-06-24 14:59:01 -0300

    Categoria comentarios

--- src/categoria/Categoria.java
+++ src/categoria/Categoria.java
@@ -4,10 +4,18 @@ package categoria;
 public abstract class Categoria {
 
 	String descripcion;
-	
+	/**
+	 * Contructor de Clase
+	 * @param descripcion tipo String
+	 */
 	Categoria (String descripcion){
 		this.descripcion = descripcion;
 	}
+	
+	/**
+	 *  Descripcion de la clase
+	 * @return String 
+	 */
 	public String getDescripcion() {
 		return this.descripcion;
 	}
--- src/categoria/CategoriaUsuario.java
+++ src/categoria/CategoriaUsuario.java
@@ -3,6 +3,10 @@ package categoria;
 
 public class CategoriaUsuario extends Categoria {
 
+	/**
+	 * Constructor de la clase Categoria Usuario hereda de {@link Categoria}
+	 * @param descripcion  Tipo String
+	 */
 	public CategoriaUsuario(String nombre) {
 		super(nombre);
 	}
